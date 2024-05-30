31e673343b567c11010b1e269318e78b0dba2775
Mankala Vaishnavi <131646799+VaishnaviMankala19@users.noreply.github.com>
2024-05-30 15:52:17 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 // import { MailchimpForm } from "./MailchimpForm";
 import navIcon1 from "../Assets/img/nav-icon1.svg";
-import navIcon2 from "../Assets/img/nav-icon2.svg";
 import navIcon3 from "../Assets/img/nav-icon3.svg";
 
 export const Footer = () => {
@@ -14,7 +13,6 @@ export const Footer = () => {
           
             <div className="social-icon">
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Mankala Vaishnavi</p>
