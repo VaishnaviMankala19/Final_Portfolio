5cd468f9b9bfa02d1138b9b2042f2d638dfb53bd
Mankala Vaishnavi <131646799+VaishnaviMankala19@users.noreply.github.com>
2024-05-30 15:50:22 +0530

    Updated NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../Assets/img/logo.png';
 import navIcon1 from '../Assets/img/nav-icon1.svg';
-import navIcon2 from '../Assets/img/nav-icon2.svg';
 import navIcon3 from '../Assets/img/nav-icon3.svg';
 
 
@@ -47,7 +46,7 @@ export const NavBar = () => {
         <span className='navbar-text'>
             <div className='social-icon'>
                 <a href='https://www.linkedin.com/in/vaishnavi-mankala-33a256252/'><img src={navIcon1} alt=''/></a>
-                // <a href='#'><img src={navIcon2} alt=''/></a>
+              
                 <a href='https://github.com/VaishnaviMankala19'><img src={navIcon3} alt=''/></a>
             </div>
             <Nav.Link href="#connect"  onClick={()=>onUpdateActiveLink('connect')}>
